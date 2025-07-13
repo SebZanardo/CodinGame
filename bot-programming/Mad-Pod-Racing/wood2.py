4ada1dec7c7c4d75bef172d85f1c646815e77d42
Seb Zanardo <sebzanardo@optusnet.com.au>
2025-07-13 19:12:45 +1000

    Mad-Pod-Racing Wood2

---
+++ bot-programming/Mad-Pod-Racing/wood2.py
@@ -0,0 +1,15 @@
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+def clamp(x: float, lo: float, hi: float) -> float:
+    return max(min(x, hi), lo)
+
+
+MAX_THRUST = 100
+MIN_THRUST = 0
+
+
+while True:
+    x, y, cx, cy, cd, ca = [int(i) for i in input().split()]
+    ox, oy = [int(i) for i in input().split()]
+    thrust = (1 - ca / 180) * MAX_THRUST
+    thrust = int(clamp(thrust, MIN_THRUST, MAX_THRUST))
+    print(f"{cx} {cy} {thrust}")
